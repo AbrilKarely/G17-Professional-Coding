9330591d1f1d0f2eb1276eeb5f4617e301cbbd9f
Abril Alcantara <abrilkarely.h@gmail.com>
2022-06-08 19:12:54 -0500

    se agregan ejercicios de funciones

--- Ciclos/index.js
+++ Ciclos/index.js
@@ -1,7 +1,43 @@
 
-let indice = 1
+//let indice = 1
 
-while (indice <= 10) {
-    console.log (indice);
-    indice = indice + 1;
-}
+//while (indice <= 10) {
+    //console.log (indice);
+    //indice = indice + 1; // indice++; suma uno a la variable//
+//}//
+
+//ejercicio1//
+// imprimir en consola su nombre 10 veces
+
+//let i = 1
+//while (i <= 10){
+    //console.log ("Abril");
+    //i++;
+//}//
+
+//ejercicio2//
+//crear un ciclo while que
+//sume de dos en dos, desde el 2 hasta el 50
+
+//let i = 2
+//while (i <= 50){
+    //document.write (i);
+    //i = 1+1;
+//}//
+
+//Segunda forma de solucionarlo abreviado
+
+//let i = 2
+//while (i <= 50){
+    //console.log (i);
+    //i += 2;
+//}//
+
+// tercera forma pero de uno en uno
+
+//let i = 2
+//while (i <= 50){
+    //console.log (i)
+    //i++; 
+//}//
+
