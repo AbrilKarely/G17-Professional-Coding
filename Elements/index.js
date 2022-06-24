ce8918180db65c3453fc159c2d4033ca4d7f1db3
Abril Alcantara <abrilkarely.h@gmail.com>
2022-06-23 21:21:51 -0500

    se agrego ejercicio de containers y de elementos

--- Elements/index.js
+++ Elements/index.js
@@ -1,14 +1,23 @@
+
 const containerJs = document.getElementById ("container")
+const button = document.getElementById("boton")
+
+
+const nameUser = "Abril"
+const edad = 27
 
 function elements(){
     let div = document.createElement ("div") //tambien se puede crear un span, section, ect//
     let data = `<div> 
     <h1 class = "text"> Hola vengo de JavaScript </h1>
-    <p>Lorem ipsum dolor sit amet consectur</p>
+    <p>Hola soy ${nameUser} y tengo ${27}</p>
     </div>`
     div.innerHTML = data
 
     containerJs.appendChild(div)
 }
 
-elements()
+button.addEventListener("click",(evento) =>{
+    evento.preventDefault()
+    elements()
+})
